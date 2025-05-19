159d06766e508e6357b41fc47729061f96362494
fedega15 <federico.galassi9@gmail.com>
2025-05-19 20:10:33 -0300

    Add Redis password configuration to webhook module

--- src/modules/webhook/webhook.module.ts
+++ src/modules/webhook/webhook.module.ts
@@ -80,6 +80,7 @@ import { StockMovement, StockMovementSchema } from '@/modules/front-systems/stoc
       connection: {
         host: process.env.REDIS_HOST,
         port: +process.env.REDIS_PORT,
+        password: process.env.REDIS_PASSWORD,
       },
       defaultJobOptions: {
         attempts: 3,
